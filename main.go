1f72b70cbeaaf13fc565132b730acc958b0c2ce3
naman-dave <namandave@duck.com>
2025-11-12 13:27:31 +0530

    feat: deployer test repo

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, World!"))
 	})
+	log.Println("Starting server on :8080")
 	http.ListenAndServe(":8080", nil)
 }
