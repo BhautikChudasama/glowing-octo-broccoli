cf5c67f58978d66913dafc38f04ec140c10f2458
Bhautik <bhautikrchudasama@gmail.com>
2025-12-02 16:49:36 +0530

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!++-- pradum"))
+		w.Write([]byte("Hello, World!++-- main - auto disabled"))
 	})
 	log.Println("Starting server on :8080")
 	http.ListenAndServe(":8080", nil)
